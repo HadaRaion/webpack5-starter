5838c081f275dd3f7d4ef9e4cfe9a1f0576b449b
Hada Raion <sweet.raion@gmail.com>
2021-12-14 02:45:33 -0800

    First commit

--- src/app.js
+++ src/app.js
@@ -1,15 +1,1 @@
 import './styles/style.scss';
-
-const elvenShieldRecipe = {
-	leatherStrips: 2,
-	ironIngot: 1,
-	refinedMoonstone: 4,
-};
-
-const eleveauntletRecipe = {
-	...elvenShieldRecipe,
-	leather: 1,
-	refinedMoonstone: 5,
-};
-console.log(elvenShieldRecipe);
-console.log(eleveauntletRecipe);
